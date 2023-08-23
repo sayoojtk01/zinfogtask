0056ca5f83624bb84afb546938d6dd0f2c53f8f7
sayoojtk01 <sayoojsayu914@gmail.com>
2023-08-23 13:51:12 +0530

    third commit

--- models/task1_delivery_charge.py
+++ models/task1_delivery_charge.py
@@ -11,28 +11,15 @@ class saleorder(models.Model):
         for order in self:
             delivery_charge = sum(line.price_subtotal for line in order.order_line) * 0.10
             order.delivery_charge = delivery_charge
+
+            # change the total with delivary charge
+
             # order.amount_total = order.amount_untaxed + delivery_charge + order.amount_tax
 
 
 
 
-    # @api.depends('amount_total')
-    # def _compute_delivery_charge(self):
-    #     for order in self:
-    #         order.delivery_charge = order.amount_total * 0.10
 
-
-
-
-# class invoiceOrder(models.Model):
-#     _inherit = 'account.move'
-#
-#     delivery_charge = fields.Float(string='Delivery Charge', compute='_compute_delivery_charge', store=True)
-#
-#     @api.depends('amount_total')
-#     def _compute_delivery_charge(self):
-#         for order in self:
-#             order.delivery_charge = order.amount_total * 0.10
 
 
 class invoiceorder(models.Model):
@@ -43,6 +30,10 @@ class invoiceorder(models.Model):
         for invoice in self:
             delivery_charge = sum(line.price_subtotal for line in invoice.invoice_line_ids) * 0.10
             invoice.delivery_charge = delivery_charge
+
+            # change the total with delivary charge
+
+
             # invoice.amount_total = invoice.amount_untaxed + delivery_charge + invoice.amount_tax
 
 
